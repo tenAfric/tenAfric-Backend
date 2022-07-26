24f5fc5954722dc189e40a2e467170fafd93eec0
DennisRono <dennisrkibet@gmail.com>
2022-07-26 13:14:07 +0300

    server blog dat

--- routes/blog.js
+++ routes/blog.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../includes/blog')
+const blogdata = require('../data/blogdata')
 
-router.post('/register', () => {
+router.get('/', (req, res, next) => {
     try {
-        
+        res.json(blogdata)
     } catch (error) {
-        
+        console.log(error);
     }
 })
 
